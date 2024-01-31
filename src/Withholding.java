e6fb4bb06e628c8c3d5dda1d074fc751f4444963
Dannysib <daniel.sibrian@csuglobal.edu>
2024-01-31 01:14:22 -0700

    Program to calculate average tax withholding weekly

---
+++ src/Withholding.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Withholding {
+    //Setting up a scanner for user input. Declaring variable //
+    // double is used since working with decimals//
+    public static void main(String[] args){
+        System.out.println("Enter your weekly income:");
+
+
+        Scanner input = new Scanner(System.in);
+        double income;
+
+        income = input.nextDouble();
+
+        //Beginning an if-else statement for the four statements//
+        // < 500$/10% , >= $500 < $1500/15%,>= $1500 < $2500/20%, >= $2500/30% //
+        if (income < 500.00){
+            income = income * .1;
+            System.out.println("Your average tax withholding is:" + " " + income);
+
+        }
+        else if (income >= 500.00 && income < 1500.00) {
+            income = income * .15;
+            System.out.println("Your average tax withholding is:" + " " + income);
+        }
+         else if (income >= 1500.00 &&  income < 2500.00){
+             income = income * .2;
+            System.out.println("Your average tax withholding is:" + " " + income);
+        }
+         else  {
+             income = income * .3;
+            System.out.println("Your average tax withholding is:" + " " + income);
+        }
+
+
+    }
+}
